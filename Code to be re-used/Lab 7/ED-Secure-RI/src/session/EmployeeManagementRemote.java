b2891d7f6ae91aaa3ba54f6db82359efa8f6e58a
Hamish Luke <hamishrluke@gmail.com>
2022-04-19 12:25:33 +1000

    Lab 7 Extracted

---
+++ Code to be re-used/Lab 7/ED-Secure-RI/src/session/EmployeeManagementRemote.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import javax.ejb.Remote;
+import entity.EmployeeDTO;
+
+
+@Remote
+public interface EmployeeManagementRemote {
+
+    boolean hasEmployee(String empid);
+    
+    boolean addEmployee(EmployeeDTO empDTO);
+
+    boolean updateEmpolyeeDetails(EmployeeDTO empDTO);
+
+    boolean updateEmployeePassword(String empid, String newPassword);
+
+    EmployeeDTO getEmployeeDetails(String empid);
+
+    boolean deleteEmployee(String empid);
+    
+    boolean removeEmployee(String empid);
+}
