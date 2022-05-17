b053f819d963978ae762c888874acd613dad7317
Melanie <102451625+axotol@users.noreply.github.com>
2022-05-17 13:22:07 +1000

    Added navigation bar

--- php_code/header.php
+++ php_code/header.php
@@ -13,6 +13,44 @@
 			}
 		?>
 		</title>
+		<link rel="stylesheet" href="stylesheet.css">
 	</head>
 
 	<body>
+	
+		<div class="navbar">
+			<a href='home.php'>Home</a>
+			<div class="dropdown">
+				<button class="dropbtn">Member
+					<i class="fa fa-caret-down"></i>
+				</button>
+				<div class="dropdown-content">
+					<a href="addmember.php">Add new member</a>
+					<a href="deactivatemember.php">Deactivate member</a>
+					<a href="searchmember.php">Search for member</a>
+					<a href="editmember.php">Edit existing member</a>
+				</div>
+			</div>
+			<div class="dropdown">
+				<button class="dropbtn">Sales Record
+					<i class="fa fa-caret-down"></i>
+				</button>
+				<div class="dropdown-content">
+					<a href="addsalesrecord.php">Add sales record</a>
+					<a href="removesalesrecord.php">Remove sales record</a>
+					<a href="searchsalesrecord.php">Search for sales record</a>
+					<a href="updatesalesrecord.php">Update sales record</a>
+				</div>
+			</div>
+			<div class="dropdown">
+				<button class="dropbtn">Report
+					<i class="fa fa-caret-down"></i>
+				</button>
+				<div class="dropdown-content">
+					<a href="viewlogsandlevels.php">Purchase Logs and Product Levels</a>
+					<a href="viewreport_daily.php">Daily</a>
+					<a href="viewreport_weekly.php">Monthly</a>
+					<a href="viewreport_monthly.php">Weekly</a>
+				</div>
+			</div>
+		</div>
